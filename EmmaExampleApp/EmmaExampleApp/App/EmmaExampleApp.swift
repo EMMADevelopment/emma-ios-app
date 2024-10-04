0e9659c3d4890ed9da6aaad3b304e030263a5290
Salva Moreno <salvador.moreno@ext.arkana.io>
2024-10-04 12:55:20 +0200

    Initial project

---
+++ EmmaExampleApp/EmmaExampleApp/App/EmmaExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmmaExampleAppApp.swift
+//  EmmaExampleApp
+//
+//  Created by Salva Moreno on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct EmmaExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
