7aeabb561cae540cbd18de6955d183fb050bc8dd
Salva Moreno <salvador.moreno@ext.arkana.io>
2024-10-04 14:56:11 +0200

    Add header in HomeView and add emma colors and logo

--- EmmaExampleApp/EmmaExampleApp/Presentation/Home/HomeView.swift
+++ EmmaExampleApp/EmmaExampleApp/Presentation/Home/HomeView.swift
@@ -9,8 +9,46 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .top) {
+            Color.white.ignoresSafeArea()
+            
+            // HEADER
+            header
+                .ignoresSafeArea()
+        }
     }
+}
+
+private var header: some View {
+    ZStack {
+        Color.emmaDark
+        
+        ZStack {
+            Image("Logo")
+                .resizable()
+                .scaledToFill()
+                .frame(maxWidth: .infinity, maxHeight: 200)
+                .padding()
+                .scaleEffect(1.9)
+                .opacity(0.2)
+                .offset(x: 9, y: 8)
+                .padding(.top, 42)
+                .clipped()
+            VStack(
+                alignment: .center,
+                spacing: -5
+            ) {
+                Text("Welcome to")
+                Text("EMMA")
+            }
+            .padding(.top, 80)
+            .foregroundStyle(Color.white)
+            .font(.system(size: 45, weight: .bold))
+            .lineLimit(1)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    .frame(height: 275)
 }
 
 #Preview {
